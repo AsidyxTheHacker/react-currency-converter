a8afb50c6c8f96d296625d9949528f6cc2c7dc4a
Adam <adammiller.2000nj@gmail.com>
2024-02-25 15:11:51 -0500

    added finished project files

--- src/CurrencyRow.jsx
+++ src/CurrencyRow.jsx
@@ -1,10 +1,22 @@
-export default function CurrencyRow() {
+import React from 'react';
+
+export default function CurrencyRow(props) {
+
+    const {
+        currencyOptions,
+        selectCurrency,
+        onChangeCurrency,
+        onChangeAmount,
+        amount
+    } = props
 
     return(
         <div>
-            <input className='input' type="number" />
-            <select name="" id="">
-                <option value="hi">hi</option>
+            <input className='input' type="number" value={amount} onChange={onChangeAmount} />
+            <select value={selectCurrency} onChange={onChangeCurrency}>
+                {currencyOptions.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
             </select>
         </div>
     )
