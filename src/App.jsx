d81b53061c195c78804b21526d8ede00a4f0d339
Adam <adammiller.2000nj@gmail.com>
2024-02-25 14:34:37 -0500

    added html in return

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import './App.css';
+import CurrencyRow from './CurrencyRow';
+
 function App() {
 
   return (
     <>
-      
+      <h1>Convert</h1>
+      <CurrencyRow/>
+      <div className='equals'>=</div>
+      <CurrencyRow/>
     </>
   )
 }
---
+++ src/CurrencyRow.jsx
@@ -0,0 +1,11 @@
+export default function CurrencyRow() {
+
+    return(
+        <div>
+            <input className='input' type="number" />
+            <select name="" id="">
+                <option value="hi">hi</option>
+            </select>
+        </div>
+    )
+};
